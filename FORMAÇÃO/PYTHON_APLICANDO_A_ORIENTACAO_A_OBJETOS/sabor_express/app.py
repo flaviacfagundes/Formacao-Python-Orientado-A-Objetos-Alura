09d6a9ae1b13494625d0e0310075c4fb8a1b7f0a
Flavia Cristina Fagundes <flaviacrisfagundes@gmail.com>
2025-10-25 16:59:20 -0300

    curso concluido com sucesso

--- FORMAÇÃO/PYTHON_APLICANDO_A_ORIENTACAO_A_OBJETOS/sabor_express/app.py
+++ FORMAÇÃO/PYTHON_APLICANDO_A_ORIENTACAO_A_OBJETOS/sabor_express/app.py
@@ -1,12 +1,12 @@
 from modelos.restaurante import Restaurante
 
-restaurante_kfc = Restaurante('KFC0', 'Fast Food')
+restaurante_kfc = Restaurante('KFC', 'Fast Food')
 restaurante_ravello = Restaurante('Ravello', 'Italiano')
 restaurante_comodoro = Restaurante('Comodoro', 'Hamburgueria')
 
 restaurante_ravello.alterar_status()
 restaurante_kfc.receber_avaliacao('Sergio', 10)
-restaurante_kfc.receber_avaliacao('Claudia', 8)
+restaurante_kfc.receber_avaliacao('Claudia', -2)
 restaurante_kfc.receber_avaliacao('Giovanna', 5)
 
 
--- FORMAÇÃO/PYTHON_APLICANDO_A_ORIENTACAO_A_OBJETOS/sabor_express/modelos/restaurante.py
+++ FORMAÇÃO/PYTHON_APLICANDO_A_ORIENTACAO_A_OBJETOS/sabor_express/modelos/restaurante.py
@@ -2,9 +2,18 @@
 from modelos.avaliacao import Avaliacao
 
 class Restaurante:
+    """Representa um restaurante e suas características."""
+
     restaurantes = []
 
-    def __init__(self, nome, categoria): 
+    def __init__(self, nome, categoria):
+        """
+        Inicializa uma instância de Restaurante.
+
+        Parâmetros:
+        - nome (`str`): O nome do restaurante.
+        - categoria (`str`): A categoria do restaurante.
+        """
         self._nome = nome.title()
         self._categoria = categoria.title()
         self._status = False
@@ -12,10 +21,12 @@ class Restaurante:
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
+        """Retorna uma representação em string do restaurante."""
         return f'Nome: {self._nome} | Categoria: {self._categoria}'
     
     @classmethod
     def listar_restaurantes(cls):
+        """Exibe uma lista formatada de todos os restaurantes."""
         print(f'{'\n-------------------------------'.ljust(31)} | {'------------------------------------'.ljust(36)} | {'------------------------------------'.ljust(36)} | {'-----------------------'}')
         print(f'{'NOME DO RESTAURANTE'.ljust(31)} | {'CATEGORIA'.ljust(36)} | {'AVALIAÇÃO'.ljust(36)} | {'STATUS'}')
         print(f'{'-------------------------------'.ljust(31)} | {'------------------------------------'.ljust(36)} | {'------------------------------------'.ljust(36)} | {'-----------------------'}')
@@ -26,19 +37,33 @@ class Restaurante:
 
     @property
     def status(self):
+        """Retorna um símbolo indicando o estado de atividade do restaurante."""
         return '✅ Ativado' if self._status else '❌ Desativado'
     
     def alterar_status(self):
+        """Alterna o estado de atividade do restaurante."""
         self._status = not self._status
 
     def receber_avaliacao(self, cliente, nota):
+        """
+        Registra uma avaliação para o restaurante.
+
+        Parâmetros:
+        - cliente (`str`): O nome do cliente que fez a avaliação.
+        - nota (`float`): A nota atribuída ao restaurante (entre 1 e 5).
+        """
+        if nota > 5:
+            nota = 5
+        elif nota < 0:
+            nota = 0
         avaliacao = Avaliacao(cliente, nota)
         self._avaliacao.append(avaliacao)
 
     @property
     def media_avaliacoes(self):
+        """Calcula e retorna a média das avaliações do restaurante."""
         if not self._avaliacao:
-            return 0
+            return 'Sem avaliação'
         soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         quantidade_notas = len(self._avaliacao)
         media = round(soma_notas / quantidade_notas, 1)
